Byeongchangw/cava

commit b7aa05ecf70352f36d3097169b040ea77bb51dce

Don't open "/dev/stdout" which does not necessarily work when spawning/forking cava from python/NodeJS, but just use the standard stdout FD instead.

cava.c
@@ -551,25 +551,28 @@ as of 0.4.0 all options are specified in config file, see in '/home/username/.co
                         fptest = open(p.raw_target, O_RDONLY | O_NONBLOCK, 0644);
 
                         if (fptest == -1) {
-                            printf("could not open file %s for writing\n", p.raw_target);
+                            fprintf(stderr, "could not open file %s for writing\n", p.raw_target);
                             exit(1);
                         }
                     } else {
                         printf("creating fifo %s\n", p.raw_target);
                         if (mkfifo(p.raw_target, 0664) == -1) {
-                            printf("could not create fifo %s\n", p.raw_target);
+                            fprintf(stderr, "could not create fifo %s\n", p.raw_target);
                             exit(1);
                         }
                         // fifo needs to be open for reading in order to write to it
                         fptest = open(p.raw_target, O_RDONLY | O_NONBLOCK, 0644);
+                        fp = open(p.raw_target, O_WRONLY | O_NONBLOCK | O_CREAT, 0644);
                     }
+                } else {
+                    fp = fileno(stdout);
+                    fprintf(stderr, "Opening stdout\n");
                 }
-                fp = open(p.raw_target, O_WRONLY | O_NONBLOCK | O_CREAT, 0644);
                 if (fp == -1) {
-                    printf("could not open file %s for writing\n", p.raw_target);
+                    fprintf(stderr, "could not open file %s for writing\n", p.raw_target);
                     exit(1);
                 }
-                printf("open file %s for writing raw output\n", p.raw_target);
+                fprintf(stderr, "open file %s for writing raw output\n", p.raw_target);
 
                 // width must be hardcoded for raw output.
                 width = 256;

commit 127ff500b0a928d4f5c718dbfe84ad548fed0ded

#409: fix bug introduced in b7aa05e, fifo was not opened it is allready existed

cava.c
@@ -547,7 +547,7 @@ as of 0.4.0 all options are specified in config file, see in '/home/username/.co
                     int fptest;
                     // checking if file exists
                     if (access(p.raw_target, F_OK) != -1) {
-                        // testopening in case it's a fifo
+                        // file exists, testopening in case it's a fifo
                         fptest = open(p.raw_target, O_RDONLY | O_NONBLOCK, 0644);
 
                         if (fptest == -1) {
@@ -562,8 +562,8 @@ as of 0.4.0 all options are specified in config file, see in '/home/username/.co
                         }
                         // fifo needs to be open for reading in order to write to it
                         fptest = open(p.raw_target, O_RDONLY | O_NONBLOCK, 0644);
-                        fp = open(p.raw_target, O_WRONLY | O_NONBLOCK | O_CREAT, 0644);
                     }
+                    fp = open(p.raw_target, O_WRONLY | O_NONBLOCK | O_CREAT, 0644);
                 } else {
                     fp = fileno(stdout);
                     fprintf(stderr, "Opening stdout\n");